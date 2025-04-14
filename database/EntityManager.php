bda65fa4173e8e5860d0e55fc798a6a02582f29c
Guuppyy <viktoryiapulish@gmail.com>
2025-04-14 09:44:16 +0300

    new

--- database/EntityManager.php
+++ database/EntityManager.php
@@ -23,25 +23,6 @@ class EntityManager
         //$this->openConnection();
     }
 
-    public function find(string $table, string $id): void
-    {
-        // Поиск записи в таблице $table с заданным идентификатором $id
-    }
-
-    public function insert(string $table, array $data): void
-    {
-        // Вставка новой записи в таблицу $table с переданными данными $data
-    }
-
-    public function update(string $table, string $id, array $data): void
-    {
-        // Обновление записи в таблице $table с идентификатором $id новыми данными $data
-    }
-
-    public function delete(string $table, string $id): void
-    {
-        // Удаление записи из таблицы $table с указанным идентификатором $id
-    }
 
     public function close(): void
     {
